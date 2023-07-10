62356022b3107f6042b57a2bc99b68c198db545e
Rafael Deluna <delunarafael@yahoo.com>
2023-07-10 02:22:00 -0400

    Completed Sprint 6 Module 4 Project

---
+++ src/App3.js
@@ -0,0 +1,26 @@
+import React, {useState,useEffect} from "react";
+import "./App.css";
+import axios from "axios";
+import NasaPhoto from "./Components/NasaPhoto";
+
+
+
+function App() {
+  const [data,setData] = useState();
+
+  useEffect(() => {
+    axios.get('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY')
+       .then(res => {
+          setData(res.data);
+       })
+       .catch(err => console.error(err))
+  },[])
+  
+  return (
+    <div className="App">
+      {data && <NasaPhoto photo = {data}/>}
+    </div>
+  );
+}
+
+export default App;
--- src/Components/NasaPhoto.js
+++ src/Components/NasaPhoto.js
@@ -1,15 +1,53 @@
 import React from "react";
+import styled, { keyframes } from "styled-components";
+
+const kf = keyframes`
+   100% {
+    opacity: 1;
+    transform: scale(1) rotateZ(0);
+   }
+`
+
+const StyledPhoto = styled.div`
+   h3 {
+      color: blue;
+      font-size: 32px;
+   }
+
+   .date {
+      color: orange;
+      font-size: 27px;
+   }
+
+   .explanation {
+      color: red;
+      font-size: 20px;
+   }
+
+   img {
+      opacity: 0;
+      transform: scale(2) rotateZ(180deg);
+      animation: ${kf} 0.5s ease-in-out forwards;
+   }
+`;
+
+
+
+
+
+
+
 
 
 
 const NasaPhoto = (props) => {
    return(
-    <div className = "nasa-photo-wrapper">
+    <StyledPhoto className = "nasa-photo-wrapper">
        <h3>{props.photo.title}</h3>
-       <p>{props.photo.date}</p>
+       <p className = "date">{props.photo.date}</p>
        <img src = {props.photo.hdurl}/>
        <p className = "explanation">{props.photo.explanation}</p>
-    </div>
+    </StyledPhoto>
    )
 }
 
